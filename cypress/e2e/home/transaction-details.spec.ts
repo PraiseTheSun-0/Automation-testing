4ec477c2be39db6dff10e1479c3870b7ba76e7dd
PraiseTheSun-0 <nikpraisethesun@gmail.com>
2023-04-16 00:46:59 +0300

    add visual tests

--- cypress/e2e/home/transaction-details.spec.ts
+++ cypress/e2e/home/transaction-details.spec.ts
@@ -8,6 +8,7 @@ describe('Transaction details tests', function () {
         cy.intercept('GET', Cypress.env('apiUrl') + "/transactions/**").as("req");
         cy.contains("Arely Kertzmann paid Edgar Johns").click();
         cy.wait('@req').then((intercept) => {
+            if (intercept.response?.statusCode === 304) return;
             expect(intercept.response?.statusCode).equal(200);
             if (intercept.response?.body.transaction.likes.findIndex((like)=>like.userId === "qywYp6hS0U") != -1)  {
                 cy.get('[data-test="transaction-like-button"]').should('be.disabled');
